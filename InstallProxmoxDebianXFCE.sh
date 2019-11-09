9821340d38ee94452f8714f336077f8f704e7d89
DOS SANTOS <danielitto91@gmail.com>
2019-11-09 12:13:32 +0100

    Update InstallProxmoxDebianXFCE.sh

--- InstallProxmoxDebianXFCE.sh
+++ InstallProxmoxDebianXFCE.sh
@@ -51,11 +51,16 @@ apt-get install xfce4 iceweasel lightdm xfce4-terminal gedit
 
 reboot
 
-wget https://github.com/Automattic/simplenote-electron/releases/download/v1.10.0/Simplenote-linux-1.10.0-amd64.deb
-ls
-apt install gconf2 -y
-dpkg -i Simplenote-linux-1.10.0-amd64.deb 
-rm Simplenote-linux-1.10.0-amd64.deb
+#wget https://github.com/Automattic/simplenote-electron/releases/download/v1.10.0/Simplenote-linux-1.10.0-amd64.deb
+#ls
+#apt install gconf2 -y
+#dpkg -i Simplenote-linux-1.10.0-amd64.deb 
+#rm Simplenote-linux-1.10.0-amd64.deb
+
+#echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | tee /etc/apt/sources.list.d/etcher.list
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+#apt update
+#apt install etcher-electron
 
 #https://github.com/Automattic/simplenote-electron/releases/tag/v1.10.0
 
