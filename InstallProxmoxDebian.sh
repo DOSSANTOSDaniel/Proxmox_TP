8b9b6421611d0f9b2ca486e6eb219248653ff7a7
DOS SANTOS <danielitto91@gmail.com>
2019-11-12 13:41:20 +0100

    Update InstallProxmoxDebian.sh

--- InstallProxmoxDebian.sh
+++ InstallProxmoxDebian.sh
@@ -14,6 +14,7 @@
 #  	Daniel DOS SANTOS < daniel.massy91@gmail.com >
 #----------------------------------------------------------------#
 
+
 apt update && apt full-upgrade -y
 
 apt install gnupg -y
@@ -27,13 +28,10 @@ routetos=$(ip route | grep '^default via' | awk '{print $3}')
 interfacewifi=$(ip link | grep ^3 | awk '{print $2}' | sed s'/://')
 interfacenet=$(ip link | grep ^2 | awk '{print $2}' | sed s'/://')
 
-echo ""
-read -p "Quelle type de connexion ? Wifi[w] ou Câble[c] : " typecon
-echo ""
-
-if [ $typecon == "c" ]
-then
-    echo "
+# Fonctions
+reseauip()
+{
+      echo "
 # This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
 
@@ -51,37 +49,35 @@ iface $interfacenet inet static
     nameserver 8.8.8.8
 " > /etc/network/interfaces
 echo $ipnet "pve1.proxmox.lan pve1" |  tee -a /etc/hosts
+}
+
+while [ : ]
+do
+echo ""
+read -p "Quelle type de connexion ? Wifi[w] ou Câble[c] : " typecon
+echo ""
+
+if [ $typecon == "c" ]
+then
+reseauip()
+echo $ipnet "pve1.proxmox.lan pve1" |  tee -a /etc/hosts
+breack
 elif [ $typecon == "w" ]
 then
-echo "
-# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
-
-source /etc/network/interfaces.d/*
-
-# The loopback network interface
-auto lo $interfacenet $interfacewifi
-iface lo inet loopback
-
-# The primary network interface
-allow-hotplug $interfacenet
-iface $interfacenet inet static
-    address $ipnet/24
-    gateway $routetos
-    nameserver 8.8.8.8
-    
+reseauip()
+echo "    
 # 2 interface wifi
 iface $interfacewifi inet static
     address $ipwifi/24
     gateway $routetos
     nameservers 8.8.8.8
 " > /etc/network/interfaces
-
 echo $ipwifi "pve1.proxmox.lan pve1" |  tee -a /etc/hosts
+breack
 else
-    echo " Erreur syntax redémarrer le script"
-    exit 1
+    echo " Erreur syntax essayez de nouveau !"
 fi
+done
 
 wget -qO - http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg | apt-key add -
 echo "deb http://download.proxmox.com/debian/pve buster pvetest" |  tee /etc/apt/sources.list.d/pve-install-repo.list
