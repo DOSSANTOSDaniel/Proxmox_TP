738106f85f064340cc96c46fb95b88be38247396
DOS SANTOS <danielitto91@gmail.com>
2019-11-12 13:58:30 +0100

    Update InstallGUIApp.sh

--- InstallGUIApp.sh
+++ InstallGUIApp.sh
@@ -75,77 +75,8 @@ apt install vim -y
 apt install glances -y
 apt install net-tools -y
 
-### Installation d'applications supplémentaires (pour mieux faire la transition de windows vers Linux)
-# installation de simplenote
-wget https://github.com/Automattic/simplenote-electron/releases/download/v1.10.0/Simplenote-linux-1.10.0-amd64.deb
-apt install gconf2 -y
-dpkg -i Simplenote-linux-1.10.0-amd64.deb
-sleep 1
-rm Simplenote-linux-1.10.0-amd64.deb
-
-# installation de etcher
-apt install zenity -y
-echo "deb https://deb.etcher.io stable etcher" | tee /etc/apt/sources.list.d/balena-etcher.list
-sleep 1
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-apt update
-apt install balena-etcher-electron -y
-
-# Installation d'AnyDesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-sleep 1
-apt update
-apt install anydesk -y
-
-# Installation de Wireshark
-apt install wireshark -y
-usermod -a -G wireshark $usertos
-chgrp wireshark /usr/bin/dumpcap
-chmod 771 /usr/bin/dumpcap
-setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
-ip link set $interfacenet promisc on
-ip link set $interfacewifi promisc on
-
-# installation de google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
-echo "
-### THIS FILE IS AUTOMATICALLY CONFIGURED ###
-# You may comment out this entry, but any other modifications may be lost.
-deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
-" > /etc/apt/sources.list.d/google-chrome.list
-apt update && apt upgrade -y
-rm google-chrome-stable_current_amd64.deb
-
-# Installation de LibreOffice
-apt install libreoffice -y
-apt install libreoffice-voikko -y
-apt install openclipart-libreoffice -y
-wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.7_all.deb
-apt install ./ttf-mscorefonts-installer_3.7_all.deb -y
-wget https://grammalecte.net/grammalecte/oxt/Grammalecte-fr-v1.5.0.oxt
-rm ttf-mscorefonts-installer_3.7_all.deb
-
-apt install vlc -y
-apt install filezilla -y
-apt install gdebi -y
-apt install gedit -y
-apt install iceweasel -y
-apt install gparted -y
-apt install screen -y
-apt install diodon -y
-apt install synaptic apt-xapian-index -y
-apt install ncdu -y
-
 ### installation de dépendances ou autre
 apt install software-properties-common -y
-
-# Pilote Radeon
-apt install libgl1-mesa-dri xserver-xorg-video-radeon -y
-
-# optimisation pour laptop
-tasksel install laptop
 
 ### Autres configurations
 #  Enlever la bannière de subscription proxmox
