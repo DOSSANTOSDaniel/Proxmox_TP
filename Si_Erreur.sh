f1f11cf1ad82c3776a79b6c6bd9e9d391a812c51
DOS SANTOS <danielitto91@gmail.com>
2019-11-12 13:25:01 +0100

    Create Si_Erreur.sh

---
+++ Si_Erreur.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Linux peut rencontrer un problème avec certaines cartes graphiques AMD, on va donc modifier les paramètres de démarrage du noyau.
+sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet spalsh"/GRUB_CMDLINE_LINUX_DEFAULT="nomodeset quiet spalsh"/' /etc/default/grub
+sleep 1
+update-grub
+
